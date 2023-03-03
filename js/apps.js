a37e51a649f3c4c340125879d9bdcab533a46055
Hemel Bourhan <hemelbourhan@gmailgit config --global user.email Bourhan>
2023-03-03 11:48:46 +0600

    create cards items in html and connect with api

--- js/apps.js
+++ js/apps.js
@@ -1,1 +1,20 @@
-console.log("connected");
+// console.log("connected");
+// load data 
+const loadData = async () =>{
+    const url =`https://openapi.programming-hero.com/api/ai/tools`
+     const res= await fetch(url);
+     const data = await res.json();
+     displayCards(data.data);
+     displayCards(data.data.tools[0].name);
+     
+}
+const displayCards = cards =>{
+    // console.log(cards);
+    const cardsContainer = document.getElementById('card-container')
+    cards.forEach(card =>{
+        console.log(card);
+        
+    })
+    
+}
+// loadData();
